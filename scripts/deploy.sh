6b2a0128183439b08d1f53c76d64172ea68b465d
chae <chsj9251@gmail.com>
2024-05-08 14:36:16 +0900

    commit_test_deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ echo "현재 구동 중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl lec1 | grep jar | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션 pid : $CURENT_PID"
+echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRNET_PID" ]; then
         echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
