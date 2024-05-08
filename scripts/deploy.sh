832d777765344365df24d63d38fd5579b5d47da5
chae <chsj9251@gmail.com>
2024-05-08 16:55:24 +0900

    commit_test_deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,14 +9,14 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl lec1 | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl lec1 | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
-if [ -z "$CURRNET_PID" ]; then
+if [ -z "$CURRENT_PID" ]; then
         echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-        echo "> kill -15 $CURRNET_PID"
+        echo "> kill -15 $CURRENT_PID"
         kill -15 $CURRENT_PID
         sleep 5
 fi
